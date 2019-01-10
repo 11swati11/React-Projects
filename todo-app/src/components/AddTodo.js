575439e68ff8f19f1612f2945904997ceb1d2d32
11swati11 <swati.choudhary1155@gmail.com>
2019-01-10 14:44:00 +0530

    AddTodo.js added

---
+++ todo-app/src/components/AddTodo.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const AddTodo = () => (
+    <div>
+        <input className="form-control" type="text" placeholder="What needs to be done?" />
+    </div>
+)
+
+export default AddTodo;
